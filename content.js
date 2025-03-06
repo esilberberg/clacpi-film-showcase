2df87fbec884fcaeea2571f66c0e00a2e4fe107f
esilberberg <ericsil218@gmail.com>
2025-03-06 18:32:24 -0500

    mobile init

---
+++ content.js
@@ -0,0 +1,177 @@
+const mainContent = {
+    scheduleLink: [
+        { en: "Schedule" },
+        { es: "Programa" }
+    ],
+    panelsLink: [
+        { en: "Panels" },
+        { es: "Ponencias" }
+    ],
+    aboutLink: [
+        { en: "About" },
+        { es: "Acerca de" }
+    ],
+    organizersLink: [
+        { en: "Organizers" },
+        { es: "Coordinadores" }
+    ],
+    title1: [
+        { en: "5th CLACPI Indigenous Film Showcase" },
+        { es: "V Muestra de Cine Indígena CLACPI" }
+    ],
+    title2: [
+        { en: "Side event of the 24th Session of the UN Permanent Forum on Indigenous Issues" },
+        { es: "Complementario al XXIV Foro Permanente para las Cuestiones Indígenas de la ONU" }
+    ],
+    registerBtn: [
+        { en: "Register" },
+        { es: "Registrar" }
+    ],
+    date: [
+        { en: "April 24 & 25 2025" },
+        { es: "24 y 25 de abril 2025" }
+    ],
+    scheduleH1: [
+        { en: "Schedule" },
+        { es: "Programa" }
+    ],
+    aboutH1: [
+        { en: "About" },
+        { es: "Acerca de" }
+    ],
+    organizersH1: [
+        { en: "Organizers" },
+        { es: "Coordinadores" }
+    ],
+    about1: [
+        { en: "Presented by the Coordinadora Latinoaméricana de Cine y Comunicación de los Pueblos Indígenas (CLACPI)." },
+        { es: "Una muestra especial de cine y video indígena latinoamericano presentada por la Coordinadora Latinoamericana de Cine y Comunicación de los Pueblos Indígenas (CLACPI)."}
+    ],
+    about2: [
+        { en: "The Center for Latin American and Caribbean Studies (CLACS) and Center for Collaborative Indigenous Research with Communities and Lands (Center CIRCL) are pleased to host a program presented by the Coordinadora Latinoaméricana de Cine y Comunicación de los Pueblos Indígenas (CLACPI) at the KJCC at NYU on April 24-25, 2025. " },
+        { es: "Es un placer para el Center for Latin American and Caribbean Studies (CLACS) Y EL Center for Collaborative Indigenous Research with Communities and Lands (Center CIRCL), llevar a cabo un evento presentado por CLACPI en el King Juan Carlos I of Spain Center (KJCC) en New York University (NYU) el 23 y 24 de abril del 2025." }
+    ],
+    about3: [
+        { en: "This Program is a Side Event of the 2025 UN Permanent Forum on Indigenous Issues." },
+        { es: "Este programa es complementario al evento Foro Permanente para las Cuestiones Indígenas del 2023 de las ONU." }
+    ],
+    about4: [
+        { en: "CLACPI was founded in 1985 and is currently the largest collective of indigenous filmmakers in Latin America. They promote the use of media as a tool of cultural affirmation and social transformation for indigenous communities. CLACPI produces and disseminates audiovisual materials about indigenous cultures and other issues of interest for their collective organizations. They also focus on grassroots media training and workshops to promote agency and self-representation among indigenous communities." },
+        { es: "CLACPI fue fundado en 1985 y es actualmente la colectiva más grande de cineastas indígenas en América Latina. Ellos promueven el uso de medios de comunicación audiovisuales como instrumentos de afirmación cultural y transformación social para las comunidades indígenas. CLACPI produce y distribuye materiales audiovisuales sobre las culturas indígenas y otras cuestiones de interés para su colectiva de organizaciones. Ellos se enfocan en entrenamientos de base audiovisuales y talleres que promueven agencia y auto-representación de las comunidades indígenas. " }
+    ]
+};
+
+const organizersContent = [
+    {
+        name: "Rocío Gómez Semanate",
+        image: "rocio.png",
+        biography: [
+            {
+                paragraph: [
+                    { en: "Rocio is the Coordinator and General Producer of WarmiCine, a creative laboratory for women, and of MuyuyCine, an audiovisual incubator working across Ecuador. These training and media production processes seek to weave, complement and highlight the presence of Indigenous Peoples.nAs cultural manager for film promotion, Rocio has received several accolades at the national level, and was awarded municipal participatory funds for interdisciplinary projects. Other national recognitions include the Mushuk Nina Award for Contribution to Indigenous Peoples Through Audiovisual Art, and Quito’s Municipal Recognition for Contribution to the Development of Culture." },
+                    { es: "Coordinadora y productora general del proyecto “WarmiCine” laboratorio de creación para mujeres, así como del MuyuyCine, un semillero audiovisual realizado en  varias localidades ecuatorianas. Ambos procesos de formación y producción audiovisual buscan  tejer, acompañar y visibilizar la presencia de los Pueblos Originarios. Gestora cultural ganadora de fondos de fomento del cine y la audiovisual a nivel nacional así como de fondos participativos municipales con proyectos interdisciplinarios. Acreedora de reconocimientos nacionales como el premio Mushuk Nina por el aporte a los pueblos y nacionalidades con el arte audiovisual y del Reconocimiento Municipal de Quito por el aporte al desarrollo de la cultura."}
+                ]
+            },
+            {
+                paragraph: [
+                    { en: "Her films include: Somos pueblo, Trenzando vida, Manuela 1945, Kitu Kara, and La Yumba. Her works are the result of a training process and a reflection on identity around contemporary Kitu Kara people and their practices as urban Indigenous people in the capital of Ecuador. She has been a field producer and presenter of the television series “Ecuador Ancestral” and worked on the feature film “Semillas de Lucha.” She is co-director of several Indigenous film festivals and exhibitions such as the recent FIC CLACPI Ecuador 2022. She trained as a social communicator, visual anthropologist and cultural manager, scriptwriter, filmmaker, audiovisual producer and popular trainer, ancestral dancer and cultural activist." },
+                    { es: "Entre su realizaciones audiovisuales destacan “Somos pueblo”  “Trenzando vida,” “Manuela 1945”  “Kitu Kara”  y “La Yumba” sus obras son el resultado de un proceso de formación y de reflexión identitaria de la presencia del pueblo Kitu Kara y sus prácticas como indígenas urbanos en la capital del Ecuador. Productora de campo y presentadora de la serie televisiva “Ecuador ancestral” y del largometraje esmeraldeño “Semillas de lucha.” Co-directora de varios festivales y muestras de cine indígena como el reciente FIC CLACPI Ecuador  2022. Comunicadora Social, Antropóloga Visual y  gestora cultural, guionista,  realizadora, productora audiovisual y formadora popular, danzante ancestral y activista de la cultura."}
+                ]
+            }
+        ]
+    },
+    {name: "Andrés Tapia Arias",
+        image: "andresTapia.png",
+        biography: [
+            {
+                paragraph: [
+                    { en: "Andrés Tapia is Political Advocacy Coordinator of CLACPI, former communication leader of the  la Confederación de Nacionalidades Indígenas de la Amazonía Ecuatoriana CONFENIAE   for two periods 2016-2023, Co-founder and former Director of Radio La Voz de la CONFENIAE (2019- 2023) as well as co-founder of the network of community communicators Lanceros Digitales. Audiovisual producer and director, he has produced and co-produced documentaries and advocacy campaigns for the defense of Amazonian territories; he co-directed the documentary Allpamanda, on the struggle of Indigenous Nations of the Ecuadorian Amazon over four decades of organizing. He has written books and articles in social sciences and communication, among them Estallido, La Rebelión de Octubre en Ecuador, in Spanish and English, with Leonidas Iza, president of CONAIE. He obtained the Tránsito Amaguaña national award in 2019 from the Organization of Ibero-American States for the creation of the Lanceros Digitales Network of communicators, and was recognized as an outstanding journalist from the “Times of Change” program of the Ombudsman of Ecuador, honoring the work of Ecuadorian journalists in the promotion and protection of human rights and nature." },
+                    { es: "Coordinador de Incidencia Política de la CLACPI, ex dirigente de comunicación de la Confederación de Nacionalidades Indígenas de la Amazonía Ecuatoriana CONFENIAE por dos períodos 2016-2023, Co fundador y ex Director de Radio La Voz de la Confeniae durante el periodo 2019-2023 así como co-fundador de la red de comunicadores comunitarios Lanceros Digitales. Productor y realizador audiovisual, ha participado en la producción y co producción de documentales y campañas de incidencia por la defensa de los territorios amazónicos; fue co -director del documental Allpamanda que narra la historia de lucha de las nacionalidades de la amazonía ecuatoriana durante cuatro décadas de vida organizativa. Autor de libros y artículos en áreas de las ciencias sociales y de la comunicación, entre ellos Estallido, La Rebelión de Octubre en Ecuador, en español y en inglés, junto a Leonidas Iza, presidente de la CONAIE. Ha obtenido los premios nacionales Tránsito Amaguaña en 2019 por parte de la Organización de Estados Iberoamericanos OEI por  la creación de la Red de comunicadores Lanceros Digitales, y el reconocimiento como periodista destacado en el marco de la convocatoria “Tiempos de Cambio” por parte de la Defensoría del Pueblo del Ecuador por la labor de periodistas ecuatorianos en la promociòn y protecciòn de derechos humanos y de la naturaleza." }
+                ]
+            }
+        ]
+    },
+    {name: "Nelly Kuiru / Moniyango (Árbol de la abundancia)",
+        image: "nelly.jpg",
+        biography: [
+            {
+                paragraph: [
+                    { en: "Nelly (whose traditional name is Moniyango) belongs to the Murui-M+n+ka (Uitoto) people of La Chorrera (department of Amazonas, Colombia). She is the general coordinator of CLACPI and director of film and audiovisual media, with studies at the National School of Film and Audiovisual Arts of Eliseo Subiela (Buenos Aires, Argentina). From 2012 to 2021, she was National Communication Commissioner of the Indigenous Peoples of the Macro-Amazon of the National Indigenous Organization of Colombia. In 2014 she was appointed as delegate of the Minister of Culture in matters of citizen’s media for the Colombian Amazon. She is co-founder of the National Communication Commission of Indigenous Peoples (CONCIP) and Ciber Amazonas (Network of Women Communicators of the Amazon Basin)." },
+                    { es: "Nelly es una mujer indígena perteneciente al pueblo indígena Murui-M+n+ka (Uitoto) de la Chorrera (departamento de Amazonas, Colombia). Es coordinadora general de CLACPI y directora de cine y medios audiovisuales de la Escuela Nacional de cine y artes audiovisuales de Elíseo Subiela (Buenos Aires, Argentina). Desde el año 2012 y hasta el 2021 fue Comisionada Nacional de Comunicación de los Pueblos Indígenas de la Macro-Amazonía de la ONIC (Organización Nacional Indígena de Colombia). En el año 2014 fue nombra delegada de la señora Ministra de Cultura en materia de medios ciudadanos para el Amazonas." }
+                ]
+            },
+            {
+                paragraph: [
+                    { en: "She is the founder and coordinator of the first School of Indigenous Communication in the Colombian Amazon (Ka + Jana Uai –La Voz de Nuestra Imagen) and the first association of Indigenous women in the Department of Amazonas (Nimaira-Amazonas). She is co-responsible for the negotiations with the Government of the Republic of Colombia, along with other representatives of Indigenous Organizations, for the creation of the Public Communication Policy of and for the Indigenous Peoples of Colombia and the National Television Plan. She studied social communication at Southern New Hampshire University (USA)." },
+                    { es: "Es co-fundadora de la Comisión Nacional de Comunicación de los Pueblos Indígenas (CONCIP) y Ciber Amazonas (Red de Mujeres Comunicadoras de la Cuenca Amazónica). Es fundadora y coordinadora de la primera Escuela de Comunicación Indígena de la Amazonia colombiana (Ka+ Jana Uai –La Voz de Nuestra Imagen-) y la primera asociación de mujeres indígenas del departamento de Amazonas (Nimaira-Amazonas). Co-responsable de las negociaciones con el Gobierno de la República de Colombia, junto con otros representantes de las Organizaciones Indígenas, para la creación de la Política Publica de comunicación de y para los Pueblos Indígenas de Colombia y del Plan Nacional de Televisión. Ha realizado estudios de comunicación social en la Southern New Hampshire University (EE.UU.)." }
+                ]
+            }
+        ]
+    },
+    {name: "Ivan Sanjinés Saavedra",
+        image: "ivanSanjines.jpg",
+        biography: [
+            {
+                paragraph: [
+                    { en: "Ivan Sanjinés Saavedra is a communicator, filmmaker, audiovisual producer and documentary filmmaker born in La Paz, Bolivia. Specialist in intercultural communication and indigenous community media. His work over the last 25 years is linked to experiences in training and transferring communication and video technologies with different indigenous peoples in Bolivia and Latin America. He is the main founder and current director of CEFREC (Foundation for the Development of Intercultural Communication), an entity that has been working within the scope of the Plurinational System of Indigenous Peasant Intercultural Communication, a broad initiative for the training, production and dissemination of original indigenous audiovisual and communicational communication." },
+                    { es: "Ivan Sanjinés Saavedra es comunicador, cineasta, productor audiovisual y documentalista nacido en La Paz, Bolivia. Especialista en comunicación intercultural y medios comunitarios indígenas. Su trabajo de los últimos 25 años está vinculado a experiencias de formación y transferencia de tecnologías de comunicación y video con distintos pueblos indígenas de Bolivia y América Latina. Es el principal fundador y actual director de CEFREC (Centro de Formación y Realización Cinematográfica), entidad que viene trabajando en el ámbito del Sistema Plurinacional de Comunicación Indígena Originario Campesino Intercultural de Bolivia, una amplia iniciativa de formación, producción y difusión de la comunicación audiovisual y comunicacional indígena originaria." }
+                ]
+            }
+        ]
+    }
+];
+
+function translateContent(language) {
+    // To translate mainContent object
+    for (const contentKey in mainContent) {
+        const element = document.getElementById(contentKey);
+
+        if (element) {
+            const translation = mainContent[contentKey].find(item => item[language]);
+            if (translation) {
+                element.textContent = translation[language];
+            }
+        }
+    }
+     // To translate Organizers object
+    const organizersDisplay = document.getElementById('organizers-display');
+    if (organizersDisplay) {
+        let organizersHTML = '';
+
+        organizersContent.forEach(organizer => {
+            let biographyHTML = '';
+            organizer.biography.forEach(bio => {
+                const translation = bio.paragraph.find(item => item[language]);
+                if (translation) {
+                    biographyHTML += `<p>${translation[language]}</p>`;
+                }
+            });
+
+            organizersHTML += `
+                <div class="organizer">
+                    <img src="2025media/people/${organizer.image}" alt="${organizer.name}">
+                    <div>
+                        <h3>${organizer.name}</h3>
+                        ${biographyHTML}
+                    </div>  
+                </div>
+            `;
+        });
+        organizersDisplay.innerHTML = organizersHTML; 
+    }
+};
+
+document.getElementById('englishBtn').addEventListener('click', function() {
+    translateContent('en');
+    hamburgerMenu.classList.remove('open');
+    navbarLinks.classList.remove('show');
+});
+
+document.getElementById('spanishBtn').addEventListener('click', function() {
+    translateContent('es');
+    hamburgerMenu.classList.remove('open');
+    navbarLinks.classList.remove('show');
+});
+
+// Initial translation (e.g., set to English by default)
+translateContent('en');
